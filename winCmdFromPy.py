a5565a265553df68d52254dbb6905f5c549cedba
packapotatoes <djk86@pitt.edu>
2017-08-26 15:36:20 -0400

    now finds paths of processes in processList.txt and saves to pathList.txt. Does not remove duplicates.

--- winCmdFromPy.py
+++ winCmdFromPy.py
@@ -11,9 +11,26 @@ def run_win_cmd(cmd):
         result.append(line)
     errcode = process.returncode
     for line in result:
-        print(line)
+        print repr(line)
     if errcode is not None:
         raise Exception('cmd %s, failed, see above for details', cmd)
-        
-        
-run_win_cmd("ls")
+    
+    return result
+
+process_list = open("processList.txt", "r")
+
+locations = []
+for process_name in process_list:
+    print "getting location for " + process_name
+    new_locations = run_win_cmd('''wmic process where 'name="'''+process_name.strip()+'''"' get ExecutablePath''')
+    
+    for line in new_locations:
+        locations.append(line)
+
+print type(locations)
+
+path_list = open("pathList.txt", "w+")
+
+for line in locations:
+    if line.strip() and not "ExecutablePath" in line:
+        path_list.write(line.strip() + "\n")
