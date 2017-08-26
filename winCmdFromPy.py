af5b754fde462b9110813f761d72bb70b2354799
packapotatoes <djk86@pitt.edu>
2017-08-26 18:25:47 -0400

    added file winCmdFromPy.py which includes a function to run a windows command via python

---
+++ winCmdFromPy.py
@@ -0,0 +1,19 @@
+import subprocess
+
+# this function runs a windows command (cmd), printing the results
+def run_win_cmd(cmd):
+    result = []
+    process = subprocess.Popen(cmd,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    for line in process.stdout:
+        result.append(line)
+    errcode = process.returncode
+    for line in result:
+        print(line)
+    if errcode is not None:
+        raise Exception('cmd %s, failed, see above for details', cmd)
+        
+        
+run_win_cmd("ls")
